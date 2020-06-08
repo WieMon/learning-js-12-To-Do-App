771db74e418d7901b8c26fe7be2253a0532fcb48
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-08 20:52:12 +0200

    Add filtering list Add examples of the filter, map, for Each methods Add removing tasks from list

--- js/script.js
+++ js/script.js
@@ -10,3 +10,36 @@ document.querySelector(`li[data-key="${index}"]`).remove();
 }
 
 document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask));
+
+/////////////////////////////////////////////////////////////////////
+// Filter
+const arr = [34, 219, 109, 2934, 12, 10, 29];
+
+const oddNumbers = arr.filter(number => number % 2);
+const evenNumbers = arr.filter(number => !(number % 2));
+const numbersBiggerThen100 = arr.filter(number => number > 100);
+
+// Map
+const double = arr.map(number => number * 2);
+const people = arr.map(number => number + " osób");
+
+//forEach
+
+arr.forEach((number, index) => arr[index] = number * 2)
+
+////////////////////////////////////////////////////////////////////
+
+const input = document.querySelector('input');
+const ul = document.querySelector('.shoppingList ul');
+const liElements = document.querySelectorAll('.shoppingList ul li');
+
+
+const searchTask = (e) => {
+  console.log(e);
+  const searchText = e.target.value.toLowerCase();
+  let tasks = [...liElements];
+  tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText))
+   ul.textContent = '';
+  tasks.forEach(li => ul.appendChild(li));
+}
+input.addEventListener('input', searchTask);
