be36da584c54ca7c21723fefed5d02c76875b94e
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-09 08:38:15 +0200

    Add adding and removing tasks

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const removeTask = (e) => {
-//e.target.parentNode.remove();
-//console.log(e.target.parentNode);
-// e.target.parentNode.style.textDecoration = 'line-through';
-// e.target.remove();
-const index = e.target.dataset.key;
-// console.log(index);
-// console.log(document.querySelector(`li[data-key="${index}"]`));
-document.querySelector(`li[data-key="${index}"]`).remove();
-}
+// const removeTask = (e) => {
+// //e.target.parentNode.remove();
+// //console.log(e.target.parentNode);
+// // e.target.parentNode.style.textDecoration = 'line-through';
+// // e.target.remove();
+// const index = e.target.dataset.key;
+// // console.log(index);
+// // console.log(document.querySelector(`li[data-key="${index}"]`));
+// document.querySelector(`li[data-key="${index}"]`).remove();
+// }
 
-document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask));
+// document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask));
 
 /////////////////////////////////////////////////////////////////////
 // Filter
@@ -29,17 +29,47 @@ arr.forEach((number, index) => arr[index] = number * 2)
 
 ////////////////////////////////////////////////////////////////////
 
-const input = document.querySelector('input');
-const ul = document.querySelector('.shoppingList ul');
-const liElements = document.querySelectorAll('.shoppingList ul li');
+// const input = document.querySelector('input');
+// const ul = document.querySelector('.shoppingList ul');
+// const liElements = document.querySelectorAll('.shoppingList ul li');
 
 
-const searchTask = (e) => {
-  console.log(e);
-  const searchText = e.target.value.toLowerCase();
-  let tasks = [...liElements];
-  tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText))
-   ul.textContent = '';
-  tasks.forEach(li => ul.appendChild(li));
+// const searchTask = (e) => {
+//   console.log(e);
+//   const searchText = e.target.value.toLowerCase();
+//   let tasks = [...liElements];
+//   tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText))
+//    ul.textContent = '';
+//   tasks.forEach(li => ul.appendChild(li));
+// }
+// input.addEventListener('input', searchTask);
+
+/////////////////////////////////////////////////////////////////////////////////
+const form = document.querySelector('.tasksToAdd form');
+const ul = document.querySelector('.tasksToAdd ul');
+const taskNumber = document.querySelector('.tasksToAdd h1 span');
+const listItems = document.getElementsByClassName('.tasksToAdd task');
+const input = document.querySelector('.tasksToAdd input');
+
+
+const removeTask = (e) => {
+  e.target.parentNode.remove();
+  taskNumber.textContent = listItems.length;
 }
-input.addEventListener('input', searchTask);
+
+const addTask = (e) => {
+  e.preventDefault();
+  const titleTask = input.value;
+  if (titleTask === '') return;
+  const task = document.createElement('li');
+  task.className = '.tasksToAdd task';
+  task.innerHTML = titleTask + '<button>Delete</button>';
+  ul.appendChild(task);
+  input.value = '';
+  taskNumber.textContent = listItems.length;
+  // console.log(taskNumber)
+  // console.log(listItems)
+  task.querySelector('button').addEventListener('click', removeTask);
+}
+
+form.addEventListener('submit', addTask);
